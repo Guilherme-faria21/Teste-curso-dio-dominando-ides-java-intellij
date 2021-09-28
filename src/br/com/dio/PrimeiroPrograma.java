269903a736aace624790d40dd754bda8c6506784
GUILHERME FARIA DE PAULA <89545486+Guilherme-faria21@users.noreply.github.com>
2021-09-28 20:14:34 -0300

    first commit intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+
+import br.com.dio.model.gato;
+
+
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       gato Gato = new gato();
+        System.out.println(Gato);
+
+        /*int a = 30;
+        int " + (a+b)); b = 50;
+        System.out.println("Hello world!);*/
+
+    }
+}
