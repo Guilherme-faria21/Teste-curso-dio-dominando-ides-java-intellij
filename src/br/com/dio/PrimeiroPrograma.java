873f8574761277571b9569aa66081a3a21a06f1c
GUILHERME FARIA DE PAULA <89545486+Guilherme-faria21@users.noreply.github.com>
2021-09-28 21:17:04 -0300

    first commit intellij

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,6 +6,7 @@ import br.com.dio.model.gato;
 
 
 public class PrimeiroPrograma {
+
     public static void main(String[] args) {
        gato Gato = new gato();
         System.out.println(Gato);
@@ -14,5 +15,39 @@ public class PrimeiroPrograma {
         int " + (a+b)); b = 50;
         System.out.println("Hello world!);*/
 
+    }
+}
+
+class livro {
+    private String nome;
+    private Integer nmrpaginas;
+
+    public livro(String nome, Integer nmrpaginas) {
+        this.nome = nome;
+        this.nmrpaginas = nmrpaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNmrpaginas() {
+        return nmrpaginas;
+    }
+
+    public void setNmrpaginas(Integer nmrpaginas){
+        this.nmrpaginas = nmrpaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", nmrpaginas=" + nmrpaginas +
+                '}';
     }
 }
